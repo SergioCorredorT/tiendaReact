a03efdfb3f9c430bc40fedf1747ae1b648c4cc41
SergioCorredorT <SergioCorredorT@gmail.com>
2024-01-02 19:39:46 +0100

    corregido bug de limpiar carrito

--- tienda/src/reducers/cartReducer.js
+++ tienda/src/reducers/cartReducer.js
@@ -1,9 +1,13 @@
-export const cartInitialState = []
+export const cartInitialState = JSON.parse(window.localStorage.getItem("cart")) || []
 
 export const CART_ACTION_TYPES = {
     ADD_TO_CART : "ADD_TO_CART",
     REMOVE_FROM_CART : "REMOVE_FROM_CART",
     CLEAR_CART: "CLEAR_CART"
+}
+
+export const updateLocalStorage = state => {
+    window.localStorage.setItem("cart", JSON.stringify(state))
 }
 
 export const cartReducer = (state, action) => {
@@ -16,26 +20,34 @@ export const cartReducer = (state, action) => {
         if (productInCartIndex >= 0) {
           const newState = structuredClone(state);
           newState[productInCartIndex].quantity += 1;
+          updateLocalStorage(newState)
           return newState;
         }
   
         //si no estaba el producto
-        return [
+        const newState = [
           ...state,
           {
             ...actionPayload, //Product
             quantity: 1,
           },
         ];
+
+        updateLocalStorage(newState)
+
+        return newState
       }
   
       case CART_ACTION_TYPES.REMOVE_FROM_CART: {
         const { id } = actionPayload;
-        return state.filter((item) => item.id !== id);
+        const newState = state.filter((item) => item.id !== id);
+        updateLocalStorage(newState)
+        return newState
       }
   
       case CART_ACTION_TYPES.CLEAR_CART: {
-        return cartInitialState;
+        updateLocalStorage([])
+        return [];
       }
     }
     return state;
