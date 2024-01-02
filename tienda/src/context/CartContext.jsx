471d3889a5fdd54b83bdaf472225473f7a0a2766
SergioCorredorT <SergioCorredorT@gmail.com>
2024-01-02 19:25:46 +0100

    usando useReducer

--- tienda/src/context/CartContext.jsx
+++ tienda/src/context/CartContext.jsx
@@ -1,40 +1,34 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer, useState } from "react";
+import { cartReducer, cartInitialState } from "../reducers/cartReducer";
 export const CartContext = createContext();
 
+function useCartReducer() {
+  const [state, dispatch] = useReducer(cartReducer, cartInitialState);
+
+  const addToCart = (product) =>
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: product,
+    });
+
+  const removeFromCart = (product) =>
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: product,
+    });
+
+  const clearCart = () => dispatch({ type: "CLEAR_CART" });
+
+  return { state, addToCart, removeFromCart, clearCart };
+}
+
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState([]);
+  const { state, addToCart, removeFromCart, clearCart } = useCartReducer();
 
-  const addToCart = (product) => {
-    //Check si está el producta ya
-    const productInCartIndex = cart.findIndex((item) => item.id === product.id);
-
-    if (productInCartIndex >= 0) {
-      const newCart = structuredClone(cart);
-      newCart[productInCartIndex].quantity += 1;
-      return setCart(newCart);
-    }
-
-    //si no está ya el producto
-    setCart((prevState) => [
-      ...prevState,
-      {
-        ...product,
-        quantity: 1,
-      },
-    ]);
-  };
-
-  const removeFromCart = (product) => {
-    setCart((prevState) => prevState.filter((item) => item.id !== product.id));
-  };
-
-  const clearCart = () => {
-    setCart([]);
-  };
   return (
     <CartContext.Provider
       value={{
-        cart,
+        cart: state,
         addToCart,
         removeFromCart,
         clearCart,
